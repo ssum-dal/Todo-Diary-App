067a3f249f738d67315198979ff722df25e3e713
ssum-dal <min8226@gmail.com>
2022-02-08 00:08:48 +0900

    [Style] 포맷팅

--- App.js
+++ App.js
@@ -2,25 +2,34 @@ import React from 'react';
 import "react-native-gesture-handler";
 import {StyleSheet} from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createStackNavigator, CardStyleInterpolators } from "@react-navigation/stack";
 import Home from "./Screens/Home";
+import Today from "./Screens/Today";
+import Upcoming from "./Screens/Upcoming";
 
 const Stack = createStackNavigator();
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} options={{headerShown: false}}/>
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
+	return (
+		<NavigationContainer>
+      		<Stack.Navigator 
+        		initialRouteName="Home"
+        		screenOptions={{
+          			cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS
+        		}}
+      		>
+        		<Stack.Screen name="Home" component={Home} options={{headerShown: false, gestureEnabled: true}}/>
+        		<Stack.Screen name="Today" component={Today} options={{headerShown: false, gestureEnabled: true}}/>
+        		<Stack.Screen name="Upcoming" component={Upcoming} options={{headerShown: false, gestureEnabled: true}}/>
+      		</Stack.Navigator>
+    	</NavigationContainer>
+  	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex : 1,
-  }
+	container: {
+    	flex : 1,
+	}
 });
 
 export default App;
