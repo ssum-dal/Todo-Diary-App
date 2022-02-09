88bf9758e5b11ee0f33645e6896932fbaffe8906
ssum-dal <min8226@gmail.com>
2022-02-09 19:51:50 +0900

    [Feat] 프로젝트 추가 기능

---
+++ Components/BottomSheet/AddProject.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native";
+import RBSheet from "react-native-raw-bottom-sheet";
+
+const s = StyleSheet.create({
+    TextInputView: {
+        borderWidth : 1,
+        borderColor: '#c2c2c2'
+    },
+    AddView: {
+        alignItems: 'center'
+    },
+    AddButton: {
+        backgroundColor: 'yellow',
+        width: '30%',
+        aspectRatio: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
+
+function AddProject({bottomSheet, addProject}) {
+    const [textValue, setTextValue] = useState('');
+    const [disabled, setDisabled] = useState(true);
+
+    console.log(bottomSheet.current)
+
+    const updateAddButton = (text) => {
+        if (text.trim().length === 0) {
+            setDisabled(true)
+        } else {
+            setDisabled(false)
+        }
+        setTextValue(text)
+    }
+
+    return (
+        <RBSheet
+            ref={bottomSheet}
+            height={400}
+            closeOnDragDown={true}
+            keyboardAvoidingViewEnabled={true}
+            customStyles={{
+                draggableIcon: {
+                    backgroundColor: "#999999"
+                },
+                container: {
+                    borderTopRightRadius:20,
+                    borderTopLeftRadius: 20,
+                }
+            }}
+        >
+            <View style={s.TextInputView}>
+                <TextInput
+                    style={{marginHorizontal: 10}}
+                    placeholder="새로운 프로젝트 이름"
+                    maxLength={20}
+                    onChangeText={(text) => {
+                        updateAddButton(text)
+                    }}
+                    value={textValue}
+                />
+            </View>
+            <View style={s.AddView}>
+            <TouchableOpacity
+                disabled={disabled}
+                onPress={() => {
+                    bottomSheet.current.close()
+                    addProject(textValue)
+                    setTextValue('')
+                }}
+            >
+                <View style={s.AddButton}>
+                    <Text>추가</Text>
+                </View>
+            </TouchableOpacity>
+            </View>
+        </RBSheet>
+    )
+}
+
+export default AddProject;
