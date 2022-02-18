f2fb378c75f572e618a76cff72bf091ead524e12
ssum-dal <min8226@gmail.com>
2022-02-19 00:01:28 +0900

    [Feat] 요일 변환 유틸 함수

--- Utils/DateFormat.js
+++ Utils/DateFormat.js
@@ -11,3 +11,10 @@ export const isToday = (date) => {
 export const dateToString = (date) => {
     return new Date(date.getTime()-(date.getTimezoneOffset() * 60000)).toISOString().slice(0, 10)
 }
+
+export const findDayofWeek = (date) => {
+    const week = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
+    const dayOfWeek = week[new Date(date).getDay()];
+    
+    return dayOfWeek;
+}
